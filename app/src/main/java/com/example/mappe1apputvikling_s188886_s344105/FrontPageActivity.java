44ed85d59330283df0d5f38a504919f767f83e36
oyvind0402 <56302124+oyvind0402@users.noreply.github.com>
2021-09-14 20:03:35 +0200

    First commit

---
+++ app/src/main/java/com/example/mappe1apputvikling_s188886_s344105/FrontPageActivity.java
@@ -0,0 +1,14 @@
+package com.example.mappe1apputvikling_s188886_s344105;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FrontPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.frontpage_activity);
+    }
+}
