e63ff56c020475dfedf334e5ac2a7acbdc700e9a
mbaan2 <mbaan2@gmail.com>
2021-09-15 17:47:55 +0200

    java app and tweaking game looks

--- app/src/main/java/com/example/mappe1apputvikling_s188886_s344105/FrontPageActivity.java
+++ app/src/main/java/com/example/mappe1apputvikling_s188886_s344105/FrontPageActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class FrontPageActivity extends AppCompatActivity {
 
@@ -12,6 +14,12 @@ public class FrontPageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.frontpage);
+
+        Button button = findViewById(R.id.start_spill);
+        button.setOnClickListener(view -> {
+            Intent intent = new Intent(view.getContext(), Spillet.class);
+            startActivity(intent);
+        });
     }
 
     public void visPreferanser(View v) {
