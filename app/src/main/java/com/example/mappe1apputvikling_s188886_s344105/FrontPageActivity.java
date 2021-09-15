539c9fc0dd062f415b9a8d779150b9e9b3cf65b2
oyvind0402 <56302124+oyvind0402@users.noreply.github.com>
2021-09-15 16:29:50 +0200

    La til koden for preferanse aktiviteten

--- app/src/main/java/com/example/mappe1apputvikling_s188886_s344105/FrontPageActivity.java
+++ app/src/main/java/com/example/mappe1apputvikling_s188886_s344105/FrontPageActivity.java
@@ -2,7 +2,9 @@ package com.example.mappe1apputvikling_s188886_s344105;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class FrontPageActivity extends AppCompatActivity {
 
@@ -10,7 +12,10 @@ public class FrontPageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.frontpage);
+    }
 
-
+    public void visPreferanser(View v) {
+        Intent intent = new Intent(this, Preferanser.class);
+        startActivity(intent);
     }
 }
