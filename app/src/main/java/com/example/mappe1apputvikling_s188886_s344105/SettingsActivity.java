abc43384cda057e66b13be336eabbbfee11667e5
mbaan2 <mbaan2@gmail.com>
2021-09-21 09:37:17 +0200

    Språk endrer seg nå automagisk

--- app/src/main/java/com/example/mappe1apputvikling_s188886_s344105/SettingsActivity.java
+++ app/src/main/java/com/example/mappe1apputvikling_s188886_s344105/SettingsActivity.java
@@ -1,26 +1,9 @@
 package com.example.mappe1apputvikling_s188886_s344105;
 
-import android.content.SharedPreferences;
-import android.content.res.Configuration;
-import android.content.res.Resources;
 import android.os.Bundle;
-import android.util.DisplayMetrics;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.preference.PreferenceManager;
 
-import java.util.Locale;
-
-public class SettingsActivity extends AppCompatActivity implements SharedPreferences.OnSharedPreferenceChangeListener {
-    @Override
-    public void onSharedPreferenceChanged(SharedPreferences preferences, String key) {
-        if(key.equals("velgSpråk")) {
-            SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
-            String land = prefs.getString("velgSpråk", "no");
-            byttLocale(land);
-            recreate();
-        }
-    }
+public class SettingsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,23 +11,5 @@ public class SettingsActivity extends AppCompatActivity implements SharedPrefere
         setContentView(R.layout.preferanser);
 
         getSupportFragmentManager().beginTransaction().replace(R.id.settings_container, new SettingsFragment()).commit();
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-    }
-
-    public void byttLocale(String land) {
-        Resources res = getResources();
-        DisplayMetrics dm = res.getDisplayMetrics();
-        Configuration cf = res.getConfiguration();
-        cf.setLocale(new Locale(land));
-        res.updateConfiguration(cf, dm);
     }
 }
--- app/src/main/java/com/example/mappe1apputvikling_s188886_s344105/SettingsFragment.java
+++ app/src/main/java/com/example/mappe1apputvikling_s188886_s344105/SettingsFragment.java
@@ -2,23 +2,56 @@ package com.example.mappe1apputvikling_s188886_s344105;
 
 import android.content.SharedPreferences;
 
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+
 
 import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 
+import java.util.Locale;
+
 
 public class SettingsFragment extends PreferenceFragmentCompat implements SharedPreferences.OnSharedPreferenceChangeListener, Preference.OnPreferenceChangeListener {
+
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         addPreferencesFromResource(R.xml.preferences);
     }
 
     @Override
-    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {}
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        if(key.equals("velgSpråk")) {
+            String land = sharedPreferences.getString("velgSpråk", "no");
+            byttLocale(land);
+        }
+        requireActivity().recreate();
+    }
+
 
     @Override
-    public boolean onPreferenceChange(Preference preference, Object o) {
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
         return false;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        getPreferenceManager().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onPause() {
+        getPreferenceManager().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
+        super.onPause();
+    }
+    public void byttLocale(String land) {
+        Resources res = getResources();
+        DisplayMetrics dm = res.getDisplayMetrics();
+        Configuration cf = res.getConfiguration();
+        cf.setLocale(new Locale(land));
+        res.updateConfiguration(cf, dm);
     }
 }
